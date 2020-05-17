47bd6ef04dae26c05679f3b276454cb167a056ba
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2020-05-17 16:53:53 +0200

    Use README for description on pypi.org

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from distutils.core import setup
 import setuptools
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'jax-random_projections',
   packages = setuptools.find_packages(),
   version = '1.0',
   license='MIT',
   description = 'sklearn\'s random projection with JAX to run on a GPU',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   author = 'Sebastian Bischoff',
   author_email = 'sebastian@salzreute.de',
   url = 'https://github.com/Baschdl/jax-random_projections',
-  download_url = 'https://github.com/Baschdl/jax-random_projections/archive/v1.0.tar.gz',
+  download_url = 'https://github.com/Baschdl/jax-random_projections/archive/v1.0.1.tar.gz',
   keywords = ['random projections', 'jax', 'GPU'],
   install_requires=[
           'sklearn',
